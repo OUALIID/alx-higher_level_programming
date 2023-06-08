fbdab075c137454559dc08859c551e4c1813167a
OUALIID <walidelhadim8@gmail.com>
2023-06-08 18:59:46 +0100

    imports functions from the file calculator_1.py 2

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-import calculator_1 as ana
+from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
-print("{} + {} = {}".format(a, b, ana.add(a, b)))
-print("{} - {} = {}".format(a, b, ana.sub(a, b)))
-print("{} * {} = {}".format(a, b, ana.mul(a, b)))
-print("{} / {} = {}".format(a, b, ana.div(a, b)))
+print("{} + {} = {}".format(a, b, add(a, b)))
+print("{} - {} = {}".format(a, b, sub(a, b)))
+print("{} * {} = {}".format(a, b, mul(a, b)))
+print("{} / {} = {}".format(a, b, div(a, b)))
