12739da2766c254aa0db0d74da53795b6f5b6e50
OUALIID <walidelhadim8@gmail.com>
2023-06-08 18:39:26 +0100

    imports functions from the file calculator_1.py

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import calculator_1 as hay
+a = 10
+b = 5
+print("{} + {} = {}".format(a, b, hay.add(a, b)))
+print("{} + {} = {}".format(a, b, hay.sub(a, b)))
+print("{} + {} = {}".format(a, b, hay.mul(a, b)))
+print("{} + {} = {}".format(a, b, hay.div(a, b)))
