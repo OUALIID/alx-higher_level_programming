b9e28602efdfc156b5d365d39fc76a0b70dda05a
OUALIID <walidelhadim8@gmail.com>
2023-06-08 18:42:50 +0100

    imports functions from the file calculator_1.py 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-import calculator_1 as hay
+import calculator_1 as ana
 a = 10
 b = 5
-print("{} + {} = {}".format(a, b, hay.add(a, b)))
-print("{} + {} = {}".format(a, b, hay.sub(a, b)))
-print("{} + {} = {}".format(a, b, hay.mul(a, b)))
-print("{} + {} = {}".format(a, b, hay.div(a, b)))
+print("{} + {} = {}".format(a, b, ana.add(a, b)))
+print("{} - {} = {}".format(a, b, ana.sub(a, b)))
+print("{} * {} = {}".format(a, b, ana.mul(a, b)))
+print("{} / {} = {}".format(a, b, ana.div(a, b)))
