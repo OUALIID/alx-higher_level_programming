23b46dd922f39ae64b098dca3365884e08ab8689
OUALIID <walidelhadim8@gmail.com>
2023-06-12 23:34:01 +0100

    tuple addition

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,11 +2,11 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_a) < 2:
         a = list(tuple_a)
-        a += [0] * (2 - len(tuple_a))        
+        a.extend([0] * (2 - len(tuple_a))        
         tuple_a = tuple(a)
     if len(tuple_b) < 2:
         b = list(tuple_b)
-        a += [0] * (2 - len(tuple_b))
+        a.extend([0] * (2 - len(tuple_b))
         tuple_b = tuple(b)
     if len(tuple_a) >= 2 and len(tuple_b) >= 2:
         c = tuple_a[0] + tuple_b[0]
