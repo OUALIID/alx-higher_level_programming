619fa06724f0bd649eb5d59a11259db9af9fc1ce
OUALIID <walidelhadim8@gmail.com>
2023-06-12 23:19:50 +0100

    tuple addition

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) < 2:
+        a = list(tuple_a)
+        a += [0] * (2 - len(tuple_a))        
+        tuple_a = tuple(a)
+    if len(tuple_b) < 2:
+        b = list(tuple_b)
+        a += [0] * (2 - len(tuple_b))
+        tuple_b = tuple(b)
+    if len(tuple_a) >= 2 and len(tuple_b) >= 2:
+        c = tuple_a[0] + tuple_b[0]
+        d = tuple_a[1] + tuple_b[1]
+    return (a, b)
+
