9dc7e5528571117cb4856884d9dffb830eaca8f4
OUALIID <walidelhadim8@gmail.com>
2023-06-13 23:23:28 +0100

    Fixing palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,9 +1,29 @@
 #include "lists.h"
 
 /**
+ * reverseList - reverses a linked list
+ * @head: double pointer to the head of the list
+ */
+void reverseList(listint_t **head)
+{
+	listint_t *prev = NULL;
+	listint_t *current = *head;
+	listint_t *next = NULL;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+	*head = prev;
+}
+
+/**
  * is_palindrome - checks if a singly linked list is a palindrome
- * @head: pointer to the head of the list
- * Return: 1 if palindrome, 0 otherwise
+ * @head: double pointer to the head of the list
+ * Return: 0 if not a palindrome, 1 if a palindrome
  */
 int is_palindrome(listint_t **head)
 {
@@ -11,7 +31,7 @@ int is_palindrome(listint_t **head)
 	listint_t *fast = *head;
 	listint_t *prev_slow = NULL;
 	listint_t *mid = NULL;
-	int is_palindrome = 1;
+	int isPalindrome = 1;
 
 	if (*head == NULL || (*head)->next == NULL)
 		return (1);
@@ -26,44 +46,25 @@ int is_palindrome(listint_t **head)
 		mid = slow;
 		slow = slow->next;
 	}
-	listint_t *second_half = slow;
-	prev_slow->next = NULL;
-	listint_t *prev = NULL;
-	listint_t *next = NULL;
-	while (second_half != NULL)
-	{
-		next = second_half->next;
-		second_half->next = prev;
-		prev = second_half;
-		second_half = next;
-	}
+	reverseList(&slow);
 	listint_t *first_half = *head;
-	second_half = prev;
+	listint_t *second_half = slow;
 	while (second_half != NULL)
 	{
 		if (first_half->n != second_half->n)
 		{
-			is_palindrome = 0;
+			isPalindrome = 0;
 			break;
 		}
 
 		first_half = first_half->next;
 		second_half = second_half->next;
 	}
-	prev = NULL;
-	next = NULL;
-	while (prev != mid)
-	{
-		next = second_half->next;
-		second_half->next = prev;
-		prev = second_half;
-		second_half = next;
-	}
+	reverseList(&slow);
 	if (mid != NULL)
-		mid->next = prev;
+		prev_slow->next = slow;
 	else
-		*head = prev;
+		*head = slow;
 
-	return is_palindrome;
+	return (isPalindrome);
 }
-
