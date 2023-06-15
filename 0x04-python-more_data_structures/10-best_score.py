c77030feccab33be0be3f2b40877e3f5a5e1d360
OUALIID <walidelhadim8@gmail.com>
2023-06-15 01:05:58 +0100

    returns a key with the biggest integer value. 1

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-def best_score(dictionary_a):
-    if dictionary_a is None:
+def best_score(a_dictionary):
+    if a_dictionary is None:
         return None
     max_value = float('-inf')
     max_key = None
-    for key, val in dictionary_a.items():
+    for key, val in a_dictionary.items():
         if val > max_value:
             max_value = val
             max_key = key
     return max_key
-
