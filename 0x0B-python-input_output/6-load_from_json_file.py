d7c778235b5acce9b3eb636a45c4ebba9a894959
OUALIID <walidelhadim8@gmail.com>
2023-07-12 23:29:43 +0100

    creates an Object from a “JSON file”:

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+0x0B. Python - Input/Output
+"""
+import json
+
+def load_from_json_file(filename):
+    """
+    open file and writes
+    """
+    with open(filename, "r", encoding='utf-8') as f:
+        load = f.read()
+        return json.loads(load)
