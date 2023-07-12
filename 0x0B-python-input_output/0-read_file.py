4a4e4b0bdceff4dc68c3d25de4b4497eb5a31636
OUALIID <walidelhadim8@gmail.com>
2023-07-12 21:14:03 +0100

    reads a text file (UTF8) and prints it to stdout:

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+0x0B. Python - Input/Output
+"""
+
+
+def read_file(filename=""):
+    """
+    open file
+    """
+    with open("my_file_0.txt", encoding="utf-8") as f:
+        opning = f.read()
+        print(opning)
