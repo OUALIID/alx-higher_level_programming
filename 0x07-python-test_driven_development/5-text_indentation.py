efd87a11d451870185df72ad2ac1ee4607344433
OUALIID <walidelhadim8@gmail.com>
2023-07-20 21:57:10 +0100

    Write a function that prints a text with 2 new lines after each of these characters: ., ? and : 2

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -6,7 +6,9 @@
 
 def text_indentation(text):
     """
-    Function that prints a text with 2 new lines after each of these characters: ., ? and :
+    Function that prints a text with 2 new lines """
+    """
+    after each of these characters: ., ? and :
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
