d0a6dd6642c669e34d71d96f942bd8c0b9488f56
OUALIID <walidelhadim8@gmail.com>
2023-07-30 23:43:00 +0100

    Write the first class Base.

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,10 +5,15 @@
 
 
 class Base:
-
+    """
+    first class Base.
+    """
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """
+        class constructor.
+        """
         if id is not None:
             self.id = id
         else:
