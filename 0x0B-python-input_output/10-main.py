e0ba8a21b692e13e6a7ab728376743d054dd7b97
OUALIID <walidelhadim8@gmail.com>
2023-07-30 19:55:55 +0100

    a class Student that defines a student by: (based on 9-student.py)

--- 0x0B-python-input_output/10-main.py
+++ 0x0B-python-input_output/10-main.py
@@ -5,8 +5,8 @@ student_1 = Student("John", "Doe", 23)
 student_2 = Student("Bob", "Dylan", 27)
 
 j_student_1 = student_1.to_json()
-j_student_2 = student_2.to_json(['first_name', 'age'])
-j_student_3 = student_2.to_json(['middle_name', 'age'])
+j_student_2 = student_2.to_json(['age', 'first_name'])
+j_student_3 = student_2.to_json(['age', 'middle_name'])
 
 print(j_student_1)
 print(j_student_2)
