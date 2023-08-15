da73346ee9f32044901f674ccfd870a2f4528747
OUALIID <walidelhadim8@gmail.com>
2023-08-16 00:02:32 +0100

    Inserts a new row into the first_table on the MySQL servere

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,6 +1,8 @@
 -- The script that inserts a new row in the table first_table.
 /*
     - INSERT INTO: Adds a new row into a table.
+    - hbtn_0c_0: The name of the database containing the table.
+    - first_table: The name of the table where you want to insert the row.
     - (id, name): Specifies the columns you're inserting data into.
     - VALUES: Indicates the values you're inserting into the columns.
     - (89, 'Best School'): The values you're inserting into the columns.
