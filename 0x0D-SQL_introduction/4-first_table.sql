46814156d4908fa2e1afefc89f7fa3e4836ec099
OUALIID <walidelhadim8@gmail.com>
2023-08-15 22:47:05 +0100

    creates a table called first_table in the current database in MySQL server.

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,11 @@
+-- The script creates a table called first_table in the current database in MySQL server.
+/*
+    - CREATE TABLE: Creates a new table.
+    - table_name: The name of the new table.
+    - id INT: An integer column named "id".
+    - name VARCHAR(256): A text column named "name" with a maximum length of 256 characters.
+*/
+CREATE TABLE table_name (
+    id INT,
+    name VARCHAR(256)
+);
