75c918878cead2c796e81212a99a5ebfc889f7f8
OUALIID <walidelhadim8@gmail.com>
2023-08-16 00:28:11 +0100

    Returns the number of records with ID = 89 in the first_table of database hbtn_0c_0 in MySQL server.

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,8 +1,8 @@
 -- The script displays the number of records in the first_table of the database.
 /*
-    - SELECT COUNT(*): Counts the number of rows in a table.
-    - AS count_89: Creates an alias called count_89 for the count result.
-    - FROM: Specifies the table from which you want to count rows.
-    - first_table: The name of the table for which you want to count rows.
+- SELECT COUNT (*): Counts the number of rows in a table.
+- AS count_89: Create a count_89 alias for the count result.
+- FROM: Specifies the table from which you want to count rows.
+- First_table: the name of the table whose rows you want to count.
 */
 SELECT COUNT(*) AS count_89 FROM first_table WHERE id = 89;
