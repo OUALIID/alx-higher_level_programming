eb9bf3f99d63b06594cb73edd5220434f4341260
OUALIID <walidelhadim8@gmail.com>
2023-08-15 23:30:27 +0100

    Inserts a new row into the first_table (database hbtn_0c_0) on the MySQL server.

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -7,5 +7,5 @@
     - VALUES: Indicates the values you're inserting into the columns.
     - (89, 'Best School'): The values you're inserting into the columns.
 */
-INSERT INTO hbtn_0c_0.first_table (id, name)
+INSERT INTO first_table (id, name)
 VALUES (89, 'Best School');
