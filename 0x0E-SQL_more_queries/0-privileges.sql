6eac53c8bc64bd2daf1beeadba42b390fc7cb2e1
OUALIID <walidelhadim8@gmail.com>
2023-08-16 19:59:41 +0100

    script that lists all MySQL user_0d_1 and user_0d_2 user privileges on localhost

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- Write a script that lists all MySQL user_0d_1 and user_0d_2 user privileges on localhost.
-SHOW GRANTS 'user_0d_1'@'localhost';
-SHOW GRANTS 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
