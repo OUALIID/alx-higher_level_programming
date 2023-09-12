7fea18073f82f5ba8674e3a9c3075408d1de7d2a
OUALIID <walidelhadim8@gmail.com>
2023-09-12 17:53:42 +0100

    Prints two arguments passed to it in the following format is

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -4,5 +4,5 @@ const args = process.argv.slice(2);
 if (args[0]) {
   console.log(args[0] + ' is ' + args[1]);
 } else {
-  console.log('undefined' +' is ' + 'undefined');
+  console.log('undefined' + ' is ' + 'undefined');
 }
