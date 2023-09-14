a5533c1cbdc8745a90cabaf51c1b40a15a7a5b2a
OUALIID <walidelhadim8@gmail.com>
2023-09-14 17:59:47 +0100

    Adding a new function that increments the value of an integer.

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,14 +1,15 @@
 #!/usr/bin/node
+
 const myObject = {
   type: 'object',
   value: 12
 };
+
 console.log(myObject);
 
-function incr () {
+myObject.incr = function () {
   myObject.value += 1;
-}
-myObject.incr = incr;
+};
 myObject.incr();
 console.log(myObject);
 myObject.incr();
