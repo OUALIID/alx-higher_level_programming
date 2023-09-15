0c5b71e86a482eb8c4e8594caf5ebd90efbec746
OUALIID <walidelhadim8@gmail.com>
2023-09-15 07:55:33 +0100

    a class Rectangle that defines a rectangle

--- 0x13-javascript_objects_scopes_closures/3-main.js
+++ 0x13-javascript_objects_scopes_closures/3-main.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const Rectangle = require('./3-rectangle');
 
-const r1 = new Rectangle(2, 3);
+const r1 = new Rectangle(10, 3);
+console.log(r1);
+console.log(r1.width);
+console.log(r1.height);
 r1.print();
-
-const r2 = new Rectangle(10, 5);
-r2.print();
