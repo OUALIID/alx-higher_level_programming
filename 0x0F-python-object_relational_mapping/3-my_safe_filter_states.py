70063616f1f6739373916e39f6ed3b82f355599a
OUALIID <walidelhadim8@gmail.com>
2023-09-17 21:53:53 +0100

    Same code as previous. But this time, safe from MySQL injections!

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Same code as previous. But this time, safe from MySQL injections!
+"""
+import MySQLdb
+import sys
+
+
+def main():
+    args = sys.argv[1:]
+    username = args[0]
+    password = args[1]
+    database = args[2]
+    state_name = args[3]
+    connection = MySQLdb.connect(
+        user=username,
+        password=password,
+        db=database,
+        host="localhost",
+        port=3306,
+
+    )
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}%' "
+                   "ORDER BY id ASC;".format(state_name))
+    for i in cursor.fetchall():
+        print(i)
+    cursor.close()
+    connection.close()
+
+
+if __name__ == "__main__":
+    main()
