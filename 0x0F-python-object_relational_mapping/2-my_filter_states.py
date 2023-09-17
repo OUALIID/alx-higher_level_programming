cb5fc373988e5c89de7d62d82e967b544cb12e87
OUALIID <walidelhadim8@gmail.com>
2023-09-17 21:34:15 +0100

    Show all values in the instance table for hbtn_0e_0_usa where name and argument match.

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Show all values in the instance table for hbtn_0e_0_usa where name and argument match.
+"""
+import MySQLdb
+import sys
+
+
+def main():
+    args = sys.argv[1:]
+    username = args[0]
+    password = args[1]
+    database = args[2]
+    state_name = args[3]
+    connection = MySQLdb.connect(
+        user=username,
+        password=password,
+        db=database,
+        host="localhost",
+        port=3306,
+        
+    )
+    cursor = connection.cursor()
+    cursor.execute( "SELECT * FROM states WHERE name LIKE BINARY '{}%' ORDER BY id ASC;".format(state_name))
+    for i in cursor.fetchall():
+        print(i)
+    cursor.close()
+    connection.close()
+
+
+if __name__ == "__main__":
+    main()
