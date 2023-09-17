ac9a624b8eae1afc39a3b60d5531536f142cb78a
OUALIID <walidelhadim8@gmail.com>
2023-09-17 21:42:36 +0100

    Show all values in the instance table for hbtn_0e_0_usa where name and argument match.

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Show all values in the instance table for hbtn_0e_0_usa where name and argument match.
+Show all values in the instance table for hbtn_0e_0_usa
+where name and argument match.
 """
 import MySQLdb
 import sys
@@ -18,10 +19,11 @@ def main():
         db=database,
         host="localhost",
         port=3306,
-        
+
     )
     cursor = connection.cursor()
-    cursor.execute( "SELECT * FROM states WHERE name LIKE BINARY '{}%' ORDER BY id ASC;".format(state_name))
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}%' "
+                   "ORDER BY id ASC;".format(state_name))
     for i in cursor.fetchall():
         print(i)
     cursor.close()
