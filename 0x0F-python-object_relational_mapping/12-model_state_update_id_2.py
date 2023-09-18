2c19604f19847733284b77af1df3b7e32c390491
OUALIID <walidelhadim8@gmail.com>
2023-09-18 10:49:21 +0100

    Change the name of the status object from the database hbtn_0e_6_usa

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Change the name of the status object from the database hbtn_0e_6_usa
+"""
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+import sys
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+             sys.argv[1],
+             sys.argv[2],
+             sys.argv[3]),
+        pool_pre_ping=True
+    )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = State(name="Louisiana")
+    update_state = session.query(State).filter_by(id=1).first()
+    session.add(update_state)
+    session.commit()
+    print(update_state.id)
+
+    session.close()
