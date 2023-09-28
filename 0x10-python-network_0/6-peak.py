b0034dc432f206679a76540efcb232a0886828e1
OUALIID <walidelhadim8@gmail.com>
2023-09-28 23:58:59 +0100

    A function that finds a vertex in a list of unsorted integers.

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,15 +4,7 @@
 
 def find_peak(list_of_integers):
     """the function find_peak"""
-    for i in range(1, len(list_of_integers) - 1):
-        prev = list_of_integers[i - 1]
-        follow = list_of_integers[i + 1]
-        curren = list_of_integers[i]
-        if curren > prev and curren > follow:
-            return curren
-    list_of = list_of_integers 
-    if (len(list_of) >= 2 and list_of[0] >= list_of[1]):
-        return list_of_integers[0]
-    if (len(list_of) >= 2 and list_of[-2] <= list_of[-1]):
-        return list_of[-1]
-    return  None
+    if list_of_integers:
+        list_of_integers.sort()
+        return list_of_integers[-1]
+    return None
