83177dde1c8afc2155eb2d50c2e59b690df44541
OUALIID <walidelhadim8@gmail.com>
2023-09-28 23:04:30 +0100

    A Bash script that takes a URL and displays all the HTTP methods that the server will accept.

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # A Bash script that takes a URL and displays all the HTTP methods that the server will accept.
 # curl -s -i -X OPTIONS "$1" | awk '/Allow:/ {print $2}'
-curl -s -i -X OPTIONS "$1" | awk -F ': ' '/Allow:/ {print $2}'
+# curl -s -i -X OPTIONS "$1" | awk -F ': ' '/Allow:/ {print $2}'
+curl -sX OPTIONS -i "$1" | grep "Allow" | awk -F ': ' '{print $2}'
