ce259d332070362bb39bae3a0e83ce36eeeb7d08
OUALIID <walidelhadim8@gmail.com>
2023-10-01 20:18:51 +0100

    A bash script that gets the status code of a request. -w for word-out.

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,5 +1,5 @@
-#!/usr/bin/bash
+#!/bin/bash
 # A bash script gets request status code. -w to output words.
-# -w "%{http_code}": This option tells `curl` to print a specific piece
-# of information after the request is completed. 
+# -w "%{http_code}": This option tells `curl` to print a specific 
+# piece of information after the request is completed. 
 curl -s -w "%{http_code}" -o /dev/null "$1"
