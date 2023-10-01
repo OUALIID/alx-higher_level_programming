da46c2654d39df70a197124f7e082e82e0d6410f
OUALIID <walidelhadim8@gmail.com>
2023-10-01 22:10:31 +0100

    A Python script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """A script that fetches https://alx-intranet.hbtn.io/status"""
-import urllib.request as ulib
+import urllib.request as urllib
 
 if __name__ == "__main__":
-    with ulib.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    with urllib.urlopen('https://alx-intranet.hbtn.io/status') as response:
         content = response.read()
     print("Body response:")
     print(f"\t- type: {type(content)}")
