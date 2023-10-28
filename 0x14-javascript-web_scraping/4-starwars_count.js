5a05d7600bac2b227d202de96f9aec80df7e4706
OUALIID <walidelhadim8@gmail.com>
2023-10-28 20:12:43 +0100

    Edition of the number of films in which the character Wedge

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,23 +2,22 @@
 const request = require('request');
 const id = process.argv.slice(2);
 const url = id[0];
-
 request(url, (err, response, body) => {
   if (err) {
-    return;
+    console.log('error_1');
   }
-
   const films = JSON.parse(body).results;
-  if (!films) {
-    return;
-  }
-
   const characterId = '18';
   let total = 0;
 
   for (const film of films) {
-    if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
-      total++;
+    if (film.characters) {
+      const date = film.characters;
+      for (const cur of date) {
+        if (cur === `https://swapi-api.alx-tools.com/api/people/${characterId}/`) {
+          total++;
+        }
+      }
     }
   }
   console.log(total);
