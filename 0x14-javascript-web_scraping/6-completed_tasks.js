418fa006a2966544261ff84f6fe43d749a0517d0
OUALIID <walidelhadim8@gmail.com>
2023-10-30 18:57:21 +0100

    A script that counts the number of completed tasks by user ID.

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,9 +1,22 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv.slice(2);
-request(url[0], (err, response) => {
-    if (err) {
-        return;
+request(url[0], (err, response, body) => {
+  if (err) {
+    console.log('No_0');
+  }
+  const dates = JSON.parse(body);
+  const userIdCounts = {};
+
+  for (const date of dates) {
+    if (date.completed === true) {
+      const userId = date.userId;
+      if (userId in userIdCounts) {
+        userIdCounts[userId]++;
+      } else {
+        userIdCounts[userId] = 1;
+      }
     }
-    console.log(response.userId)
-})
+  }
+  console.log(userIdCounts);
+});
