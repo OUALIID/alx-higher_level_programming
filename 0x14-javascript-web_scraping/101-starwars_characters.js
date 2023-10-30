dd5d7184b64fb8075e3ba9d1e5e1a8b0ffc63e50
OUALIID <walidelhadim8@gmail.com>
2023-10-30 21:12:32 +0100

    Print all Star Wars characters

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,11 +1,17 @@
 #!/usr/bin/node
 const request = require('request');
 const id = process.argv.slice(2);
-const url = `https://swapi-api.alx-tools.com/api/people/${id[0]}/`;
+const url = `https://swapi-api.alx-tools.com/api/films/${id[0]}/`;
 request(url, (err, response, body) => {
   if (err) {
     return;
   }
-    console.log(JSON.parse(dateBody).name);
-    
+  const dates = JSON.parse(body).characters;
+  dates.forEach(date => {
+    request(date, (dateErr, dateResponse, dateBody) => {
+      if (date) {
+        console.log(JSON.parse(dateBody).name);
+      }
+    });
+  });
 });
