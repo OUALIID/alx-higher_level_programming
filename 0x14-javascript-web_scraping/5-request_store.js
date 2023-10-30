5381e8490449b4202d61b3f2a52d23d571ae1281
OUALIID <walidelhadim8@gmail.com>
2023-10-30 16:36:46 +0100

    A script that obtains the contents of a web page and stores it in a file.

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,2 +1,14 @@
 #!/usr/bin/node
-const request = require('')
+const fs = require('fs');
+const request = require('request');
+const file = process.argv.slice(2);
+const url = file
+request(url[0], (err, response, body) => {
+  if (err) {
+    return;
+  }
+  fs.writeFile(file[1], body, (date) => {
+    process.exit(1);
+  })
+  return;
+})
