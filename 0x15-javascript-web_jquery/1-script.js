8ca274693c95144d18077ad82ee32af3bf94e45c
OUALIID <walidelhadim8@gmail.com>
2023-11-01 23:58:11 +0100

    Update the <header> element's text color to red without using document.querySelector.

--- 0x15-javascript-web_jquery/1-script.js
+++ 0x15-javascript-web_jquery/1-script.js
@@ -1,1 +1,6 @@
 #!/usr/bin/node
+/* Update the <header> element's text color to red without using document.querySelector. */
+/* global $ */
+$(document).ready(function () {
+  $('header').css('color', '#FF0000');
+});
