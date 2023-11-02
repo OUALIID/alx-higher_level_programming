6c4450044cd2abd862059400fcb768156ae0f6fc
OUALIID <walidelhadim8@gmail.com>
2023-11-02 10:57:51 +0100

    A JavaScript script that fetches the url and displays the hello value from that fetch in the HTML DIV#hello tag.

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 /* A JavaScript script that fetches the url and displays the hello value from that fetch in the HTML DIV#hello tag. */
 /* global $ */
-const url = 'https://hellosalut.stefanbohacek.dev/?lang=fr'
-$(document).ready(function() {
-    $.get(url, function(data) {
-        $('#hello').text(data.hello);
-    });
+const url = 'https://hellosalut.stefanbohacek.dev/?lang=fr';
+$(document).ready(function () {
+  $.get(url, function (data) {
+    $('#hello').text(data.hello);
+  });
 });
