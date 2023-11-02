fdc085675bf13fe2564d8ccdfa8b5d991d15fb9f
OUALIID <walidelhadim8@gmail.com>
2023-11-02 09:19:44 +0100

    A JavaScript program that adds a <li> element to the list when the user clicks the DIV#add_item tag.

--- 0x15-javascript-web_jquery/5-script.js
+++ 0x15-javascript-web_jquery/5-script.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+/* A JavaScript program that adds a <li> element to the list when the user clicks the DIV#add_item tag. */
+/* global $ */
+$(document).ready(function () {
+  $('#add_item').click(function () {
+    $('ul.my_list').append($('<li>Item</li>'));
+  });
+});
