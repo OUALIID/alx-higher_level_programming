e04f622cecc0701f27cab4e78f48683a537a8f15
OUALIID <walidelhadim8@gmail.com>
2023-11-02 08:43:05 +0100

    A JavaScript program that adds the red class to the <header> element when the user clicks on the DIV#red_header tag

--- 0x15-javascript-web_jquery/3-script.js
+++ 0x15-javascript-web_jquery/3-script.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+/* A JavaScript program that adds the red class to the <header> element when the user clicks on the DIV#red_header tag. */
+/* global $ */
+$(document).ready(function() {
+    $('#red_header').click(function() {
+        $('header').addClass('red');
+   });
+});
