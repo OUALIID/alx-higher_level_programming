754d423021ed0cf4e79f826f61b601f21ff3c42b
OUALIID <walidelhadim8@gmail.com>
2023-11-02 10:50:44 +0100

    A JavaScript program that fetches the titles of all movies and inserts them using the URL.

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 /* A JavaScript program that fetches the titles of all movies and inserts them using the URL. */
-/* global $ */
+/* global $ */    
+const url = 'https://swapi-api.alx-tools.com/api/films/?format=json';
 $(document).ready(function () {
-  const Url = 'https://swapi-api.alx-tools.com/api/films/?format=json';
-  $.get(Url, function (data) {
-    $.each(data.results, function (index, movie) {
-      $('#list_movies').append($('<li>').text(movie.title));
+    $.get(url, function (data) {
+        $.each(data.results, function (index, movie) {
+            $('#list_movies').append($('<li>').text(movie.title));
+        });
     });
-  });
 });
