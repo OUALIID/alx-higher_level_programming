ac8e14db5c2e7026b563091e603482b45939752b
OUALIID <walidelhadim8@gmail.com>
2023-11-02 10:05:39 +0100

    A JavaScript program that fetches the character name from a URL.

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,3 +1,9 @@
 #!/usr/bin/node
-/*  */
+/* A JavaScript program that fetches the character name from a URL. */
 /* global $ */
+const Url = 'https://swapi-api.alx-tools.com/api/people/5/?format=json';
+$(document).ready(function () {
+  $.get(Url, function (data) {
+    $('#character').text(data.name);
+  });
+});
