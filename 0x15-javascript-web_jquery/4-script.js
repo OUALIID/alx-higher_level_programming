81c3913659099c29dc5ba471b36c78bf7fc8d1dd
OUALIID <walidelhadim8@gmail.com>
2023-11-02 09:07:57 +0100

    A JavaScript program that always switches the class of a <header> element to one: red or green.

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,12 +1,13 @@
 #!/usr/bin/node
 /** A JavaScript program that always switches the class of a <header> element to one: red or green. */
-$(document).ready(function() {
-    $('#toggle_header').click(function() {
-        const header = $('header');
-        if (header.hasClass('red')) {
-            header.removeClass('red').addClass('green');
-        } else {
-            header.removeClass('green').addClass('red');
-        }
-    });
+/* global $ */
+$(document).ready(function () {
+  $('#toggle_header').click(function () {
+    const header = $('header');
+    if (header.hasClass('red')) {
+      header.removeClass('red').addClass('green');
+    } else {
+      header.removeClass('green').addClass('red');
+    }
+  });
 });
