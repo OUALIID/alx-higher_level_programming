f0994f10aa531cbdf2c0c7bffd7a849381d48ca8
OUALIID <walidelhadim8@gmail.com>
2023-11-02 08:23:00 +0100

    A JavaScript script that updates the header element's text color to red when the user clicks the DIV#red_header tag

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,1 +1,6 @@
 #!/usr/bin/node
+/* A JavaScript script that updates the header element's text color to red when the user clicks the DIV#red_header tag */
+/* global $ */
+$(document).ready(function () {
+  $('#red_header').css('color', '#FF0000');
+});
