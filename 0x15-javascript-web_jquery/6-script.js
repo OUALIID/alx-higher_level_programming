b60fce87bc4c1ba03e4ffd31af07a2092eff0274
OUALIID <walidelhadim8@gmail.com>
2023-11-02 09:33:01 +0100

    A JavaScript program that updates the text of the <header> element to New Headerc! When user clicks on DIV#update_header.

--- 0x15-javascript-web_jquery/6-script.js
+++ 0x15-javascript-web_jquery/6-script.js
@@ -1,3 +1,8 @@
 #!/usr/bin/node
-/*  */
+/* A JavaScript program that updates the text of the <header> element to New Header!!! When user clicks on DIV#update_header. */
 /* global $ */
+$(document).ready(function () {
+  $('#update_header').click(function () {
+    $('header').text('New Header!!!');
+  });
+});
