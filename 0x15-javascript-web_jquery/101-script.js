4f7e63d564c2b4c7ee2710a774580842b8bff17f
OUALIID <walidelhadim8@gmail.com>
2023-11-02 22:40:01 +0100

    A JavaScript program that adds, removes, and clears LI items from the list when the user clicks.

--- 0x15-javascript-web_jquery/101-script.js
+++ 0x15-javascript-web_jquery/101-script.js
@@ -1,3 +1,16 @@
 #!/usr/bin/node
-/*  */
+/* A JavaScript program that adds, removes, and clears LI items from the list when the user clicks. */
 /* global $ */
+$(document).ready(function () {
+  $('#add_item').click(function () {
+    $('UL.my_list').append($('<li>Item</li>'));
+  });
+
+  $('#remove_item').click(function () {
+    $('UL.my_list li:last-child').remove();
+  });
+
+  $('#clear_list').click(function () {
+    $('UL.my_list li').remove();
+  });
+});
