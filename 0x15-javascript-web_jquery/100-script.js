6910c648a484bf1f78728c83476a6231006a7b82
OUALIID <walidelhadim8@gmail.com>
2023-11-02 11:14:30 +0100

    A JavaScript program that updates the text color of a <header> element to red.

--- 0x15-javascript-web_jquery/100-script.js
+++ 0x15-javascript-web_jquery/100-script.js
@@ -1,3 +1,8 @@
 #!/usr/bin/node
-/*  */
-/* global $ */
+/* A JavaScript program that updates the text color of a <header> element to red. */
+document.addEventListener('DOMContentLoaded', function () {
+  const header = document.querySelector('header');
+  if (header) {
+    header.style.color = '#FF0000';
+  }
+});
