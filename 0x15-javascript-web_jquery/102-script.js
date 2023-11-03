2ee168b89c3092e3d5a6b0a41274f97e41aaa549
OUALIID <walidelhadim8@gmail.com>
2023-11-03 02:38:46 +0100

    A JavaScript script that fetches and prints how to say hello depending on the language.

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,3 +1,13 @@
 #!/usr/bin/node
-/*  */
+/* A JavaScript script that fetches and prints how to say "hello" depending on the language. */
 /* global $ */
+$(document).ready(function () {
+  $('#btn_translate').click(function () {
+    const languageCode = $('#language_code').val();
+    const url = `https://hellosalut.stefanbohacek.dev/?lang=${languageCode}`;
+    $('#hello').empty();
+    $.get(url, function (data) {
+      $('#hello').append(data.hello);
+    });
+  });
+});
