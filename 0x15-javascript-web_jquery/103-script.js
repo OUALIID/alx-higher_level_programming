751b03778ce96c19275513c4b1a56d4bd2592a40
OUALIID <walidelhadim8@gmail.com>
2023-11-03 03:04:42 +0100

    JavaScript script that fetches and prints how to say Hello when the user clicks #btn_translate or presses ENTER

--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -1,3 +1,19 @@
 #!/usr/bin/node
-/*  */
+/* JavaScript script that fetches and prints how to say "Hello" when the user clicks #btn_translate or presses ENTER */
 /* global $ */
+$(document).ready(function () {
+  $('#language_code').on('keypress', function (event) {
+    if (event.which === 13) {
+      $('#btn_translate').click();
+    }
+  });
+
+  $('#btn_translate').click(function () {
+    const languageCode = $('#language_code').val();
+    const url = `https://hellosalut.stefanbohacek.dev/?lang=${languageCode}`;
+    $('#hello').empty();
+    $.get(url, function (date) {
+      $('#hello').append(date.hello);
+    });
+  });
+});
